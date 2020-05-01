56e1fa2d4da314430623f64952ae0bfed935a14c
Pedja <pedjadjape97@hotmail.rs>
2020-05-01 23:42:14 +0200

    Issue #11 Add status property to overview table

--- src/app/components/overview/overview.component.ts
+++ src/app/components/overview/overview.component.ts
@@ -9,17 +9,18 @@ export interface PeriodicElement {
   position: number;
   createdAt: string;
   updatedAt: string;
+  status: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Informacione tehnologije', semester: 'I', createdAt: '2/2/2020', updatedAt: '3/3/2000'},
-  {position: 2, name: 'Informacione tehnologije', semester: 'II', createdAt: '2/2/2020', updatedAt: '3/3/2000'},
-  {position: 3, name: 'Informacione tehnologije', semester: 'III', createdAt: '2/2/2020', updatedAt: '3/3/2000'},
-  {position: 4, name: 'Informacione tehnologije', semester: 'IV', createdAt: '2/2/2020', updatedAt: '3/3/2000'},
-  {position: 5, name: 'Informacione tehnologije', semester: 'V', createdAt: '2/2/2020', updatedAt: '3/3/2000'},
-  {position: 6, name: 'Informacione tehnologije', semester: 'VI', createdAt: '2/2/2020', updatedAt: '3/3/2000'},
-  {position: 7, name: 'Informacione tehnologije', semester: 'VII', createdAt: '2/2/2020', updatedAt: '3/3/2000'},
-  {position: 8, name: 'Informacione tehnologije', semester: 'VIII', createdAt: '2/2/2020', updatedAt: '3/3/2000'}
+  {position: 1, name: 'Informacione tehnologije', semester: 'I', createdAt: '2/2/2020', updatedAt: '3/3/2000', status: 'Active'},
+  {position: 2, name: 'Informacione tehnologije', semester: 'II', createdAt: '2/2/2020', updatedAt: '3/3/2000', status: 'Active'},
+  {position: 3, name: 'Informacione tehnologije', semester: 'III', createdAt: '2/2/2020', updatedAt: '3/3/2000', status: 'Active'},
+  {position: 4, name: 'Informacione tehnologije', semester: 'IV', createdAt: '2/2/2020', updatedAt: '3/3/2000', status: 'Active'},
+  {position: 5, name: 'Informacione tehnologije', semester: 'V', createdAt: '2/2/2020', updatedAt: '3/3/2000', status: 'Active'},
+  {position: 6, name: 'Informacione tehnologije', semester: 'VI', createdAt: '2/2/2020', updatedAt: '3/3/2000', status: 'Active'},
+  {position: 7, name: 'Informacione tehnologije', semester: 'VII', createdAt: '2/2/2020', updatedAt: '3/3/2000', status: 'Active'},
+  {position: 8, name: 'Informacione tehnologije', semester: 'VIII', createdAt: '2/2/2020', updatedAt: '3/3/2000', status: 'Active'}
 ];
 
 @Component({
@@ -30,7 +31,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class OverviewComponent implements OnInit {
 
   constructor() { }
-  displayedColumns: string[] = ['name', 'semester', 'createdAt', 'updatedAt' , 'actions'];
+  displayedColumns: string[] = ['name', 'semester', 'createdAt', 'updatedAt', 'status' , 'actions'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   @ViewChild(MatSort, {static: true}) sort: MatSort;

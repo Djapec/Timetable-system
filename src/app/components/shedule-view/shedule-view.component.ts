182a4cad380699e648dcc0252e3fd8a3b3761f3c
Pedja <pedjadjape97@hotmail.rs>
2020-05-06 01:06:52 +0200

    Issue #13 Add shedule-view component
    Issue #13 add Create new button to overview component.
    Issue #13 fix route

---
+++ src/app/components/shedule-view/shedule-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shedule-view',
+  templateUrl: './shedule-view.component.html',
+  styleUrls: ['./shedule-view.component.css']
+})
+export class SheduleViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
